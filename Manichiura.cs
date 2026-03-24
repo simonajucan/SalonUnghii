037d1b03dbfc179a8aea1f656dd9a2d1a9e00904
simonajucan <simona.jucan@student.usv.ro>
2026-03-24 17:40:46 +0200

    Add files via upload

---
+++ Manichiura.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalonUnghii.Models
+{
+    public class Manichiura
+    {
+        public int Id { get; set; }
+        public string Tip { get; set; } = "";
+        public decimal Pret { get; set; }
+        public int DurataMinute { get; set; }
+    }
+}
