9ab8dc96e2f68af601b2fb374d7f2c91fd95aec7
simonajucan <simona.jucan@student.usv.ro>
2026-03-24 17:41:21 +0200

    Add files via upload

---
+++ Produs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalonUnghii.Models
+{
+    public class Produs
+    {
+        public int Id { get; set; }
+        public string Nume { get; set; } = "";
+        public int Cantitate { get; set; }
+        public decimal Pret { get; set; }
+    }
+}
