08bf4cf41f1c110de787ff23514e1aec9f9d90f0
simonajucan <simona.jucan@student.usv.ro>
2026-03-24 17:41:46 +0200

    Add files via upload

---
+++ Programare.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalonUnghii.Models
+{
+    public class Programare
+    {
+        public int Id { get; set; }
+        public string NumeClienta { get; set; } = "";
+        public DateTime Data { get; set; }
+        public string TipManichiura { get; set; } = "";
+    }
+}
