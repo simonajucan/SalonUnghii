43e0fef3b7e885032fd866b5188850fb738091b2
Simona <simona.jucan@student.usv.ro>
2026-04-08 09:31:03 +0300

    tema lab 6

---
+++ SalonUnghii.WPF/App.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows;
+
+namespace SalonUnghii.WPF
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ SalonUnghii.WPF/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SalonUnghii.WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
